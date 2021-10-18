2d1c67028a1a3f42a1d535369bf2fb3dab564958
4i4ek <4i4ekov@gmail.com>
2021-10-18 13:55:16 +0300

    commit2

--- jwts/jwts/settings.py
+++ jwts/jwts/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b^_4q6a(zd9u)@phv3%1c++d1t215#50+vwef+&nv#qo5c2m5('
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
